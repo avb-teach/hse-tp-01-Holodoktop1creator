5094d92dbcc2eaeadb68352f3397bee904f902ce
Исагаджи <Isagadzhi2006@mail.ru>
2025-04-30 20:44:06 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-if [ "$1" = "--max_depth" ]; then
-    max_depth=$2
-    input=$3
-    output=$4
-else
-    max_depth=""  # Без ограничения глубины
-    input=$1
-    output=$2
-fi
-
-if [ -n "$max_depth" ]; then
-    find "$input" -mindepth 1 -maxdepth "$max_depth" -type f -exec cp {} "$output" \;
-else
-    find "$input" -type f -exec cp {} "$output" \;
-fi
+input=$1
+output=$2
+find "$input" -type f -exec cp {} "$output" \;
+#ссылка на статью откуда брал части кода - https://stackoverflow.com/questions/33809986/comparing-file-names-and-md5sums-of-multiple-files-in-linux
